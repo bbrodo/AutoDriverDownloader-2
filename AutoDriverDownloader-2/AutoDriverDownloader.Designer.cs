b691af0845286db1881e9bef4b760dbfa3710350
bbrodo <15292@coderacademy.edu.au>
2025-06-14 16:46:19 +1000

    init: Ported AutoDriverDownloader to Windows Form

---
+++ AutoDriverDownloader-2/AutoDriverDownloader.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace AutoDriverDownloader_2
+{
+    partial class AutoDriverDownloader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutoDriverDownloader));
+            this.Title = new System.Windows.Forms.Label();
+            this.infoBox = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.driverListBox = new System.Windows.Forms.CheckedListBox();
+            this.driverListLbl = new System.Windows.Forms.Label();
+            this.downloadBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Location = new System.Drawing.Point(240, 9);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(301, 29);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "Auto Driver Downloader 2";
+            // 
+            // infoBox
+            // 
+            this.infoBox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.infoBox.Location = new System.Drawing.Point(29, 94);
+            this.infoBox.MinimumSize = new System.Drawing.Size(285, 310);
+            this.infoBox.Name = "infoBox";
+            this.infoBox.Padding = new System.Windows.Forms.Padding(3);
+            this.infoBox.Size = new System.Drawing.Size(285, 310);
+            this.infoBox.TabIndex = 1;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(28, 60);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(110, 31);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // driverListBox
+            // 
+            this.driverListBox.FormattingEnabled = true;
+            this.driverListBox.Location = new System.Drawing.Point(586, 94);
+            this.driverListBox.Margin = new System.Windows.Forms.Padding(0);
+            this.driverListBox.MinimumSize = new System.Drawing.Size(202, 310);
+            this.driverListBox.Name = "driverListBox";
+            this.driverListBox.Size = new System.Drawing.Size(202, 310);
+            this.driverListBox.TabIndex = 3;
+            // 
+            // driverListLbl
+            // 
+            this.driverListLbl.AutoSize = true;
+            this.driverListLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.driverListLbl.Location = new System.Drawing.Point(581, 66);
+            this.driverListLbl.Name = "driverListLbl";
+            this.driverListLbl.Size = new System.Drawing.Size(181, 25);
+            this.driverListLbl.TabIndex = 4;
+            this.driverListLbl.Text = "Detected Driver List";
+            // 
+            // downloadBtn
+            // 
+            this.downloadBtn.Location = new System.Drawing.Point(586, 410);
+            this.downloadBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.downloadBtn.Name = "downloadBtn";
+            this.downloadBtn.Size = new System.Drawing.Size(202, 28);
+            this.downloadBtn.TabIndex = 5;
+            this.downloadBtn.Text = "Download Selected";
+            this.downloadBtn.UseVisualStyleBackColor = true;
+            this.downloadBtn.Click += new System.EventHandler(this.downloadBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(332, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 310);
+            this.label1.TabIndex = 6;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // AutoDriverDownloader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(814, 457);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.downloadBtn);
+            this.Controls.Add(this.driverListLbl);
+            this.Controls.Add(this.driverListBox);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.infoBox);
+            this.Controls.Add(this.Title);
+            this.Name = "AutoDriverDownloader";
+            this.Text = "Auto Driver Downloader 2";
+            this.Load += new System.EventHandler(this.AutoDriverDownloader_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Label infoBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.CheckedListBox driverListBox;
+        private System.Windows.Forms.Label driverListLbl;
+        private System.Windows.Forms.Button downloadBtn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AutoDriverDownloader-2/AutoDriverDownloader.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using static System.Net.WebRequestMethods;
+using System.Management;
+
+namespace AutoDriverDownloader_2
+{
+    public partial class AutoDriverDownloader : Form
+    {
+        public AutoDriverDownloader()
+        {
+            InitializeComponent();
+        }
+
+        private void AutoDriverDownloader_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            infoBox.Text = "Starting...\n";
+            List<string> driverList = new List<string>();
+            //Get CPU Name
+            Console.WriteLine("=== CPU Information ===");
+            infoBox.Text += "=== CPU Information ===";
+            using (var cpuSearcher = new ManagementObjectSearcher("select * from Win32_Processor"))
+            {
+                foreach (ManagementObject obj in cpuSearcher.Get())
+                {
+                    string name = obj["Name"]?.ToString();
+                    string dedicatedCpu = name;
+                    if (name.Contains("AMD"))
+                    {
+                        driverList.Add("AMD");
+                    }
+                    else
+                    {
+                        driverList.Add("INTEL");
+                    }
+                    Console.WriteLine("Detected Driver: " + driverList[driverList.Count() - 1]);
+                    infoBox.Text += "\nDetected Driver: " + driverList[driverList.Count() - 1];
+                    Console.WriteLine("CPU Name: " + name);
+                    infoBox.Text += "\nCPU Name: " + name;
+                }
+            }
+
+            // Get GPU Name
+            Console.WriteLine("\n=== GPU Information ===");
+            infoBox.Text += "\n=== GPU Information ===";
+            using (var gpuSearcher = new ManagementObjectSearcher("select * from Win32_VideoController"))
+            {
+                string dedicatedGpu = null;
+                foreach (ManagementObject obj in gpuSearcher.Get())
+                {
+                    string name = obj["Name"]?.ToString();
+                    if (name.Contains("NVIDIA"))
+                    {
+                        driverList.Add("NVIDIA");
+                        dedicatedGpu = name;
+                        break;
+                    }
+                    else if (name.Contains("AMD") && !driverList.Contains("AMD"))
+                    {
+                        driverList.Add("AMD");
+                        dedicatedGpu = name;
+                    }
+                }
+                Console.WriteLine("Detected Driver: " + driverList[driverList.Count() - 1]);
+                infoBox.Text += "\nDetected Driver: " + driverList[driverList.Count() - 1];
+                Console.WriteLine("GPU Name: " + dedicatedGpu);
+                infoBox.Text += "\nGPU Name: " + dedicatedGpu;
+            }
+
+            // Gets Peripheral Devices
+            Console.WriteLine("\n=== Input Devices with Vendor Info ===");
+            infoBox.Text += "\n=== Input Devices with Vendor Info ===";
+
+            using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity"))
+            {
+                foreach (ManagementObject device in searcher.Get())
+                {
+                    string name = device["Name"]?.ToString().ToLower();
+                    string deviceId = device["DeviceID"]?.ToString();
+
+                    // Checks if device is a mouse or keyboard
+                    if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(deviceId) && (name.Contains("mouse") ||
+                         name.Contains("keyboard")))
+                    {
+                        Console.WriteLine($"Device: {name}");
+                        infoBox.Text += $"\nDevice: {name}";
+
+                        // Checks vendor map for a match
+                        var match = Regex.Match(deviceId, @"VID_([0-9A-F]{4})&PID_([0-9A-F]{4})", RegexOptions.IgnoreCase);
+                        if (match.Success)
+                        {
+                            string vid = match.Groups[1].Value.ToUpper();
+                            string pid = match.Groups[2].Value.ToUpper();
+                            Console.WriteLine($"  VID: {vid}, PID: {pid}");
+                            infoBox.Text += $"\n  VID: {vid}, PID: {pid}";
+
+                            string vendor = GetVendorName(vid);
+                            // adds drivers to driver list
+                            //WIP NEED TO MAKE A METHOD TO AUTOMATE THIS
+                            if (vendor != null)
+                            {
+                                if (vendor.Contains("LOGITECH") && !driverList.Contains("LOGITECH"))
+                                {
+                                    driverList.Add("LOGITECH");
+                                }
+                                else if (vendor.Contains("RAZER") && !driverList.Contains("RAZER"))
+                                {
+                                    driverList.Add("RAZER");
+                                }
+                                Console.WriteLine($"  Manufacturer: {vendor}");
+                                infoBox.Text += $"\n  Manufacturer: {vendor}";
+                            }
+                            else
+                            {
+                                Console.WriteLine("  Manufacturer: Unknown");
+                                infoBox.Text += "\n  Manufacturer: Unknown";
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Detected Driver: " + driverList[driverList.Count() - 1]);
+            infoBox.Text += "\nDetected Driver: " + driverList[driverList.Count() - 1];
+            //sends list of drivers to the download handler
+            string[] driverArray = driverList.ToArray();
+            driverListBox.Items.AddRange(driverArray);
+            
+        }
+
+        // Vendor map used to decode VID of peripherals to get name of vendor
+        static string GetVendorName(string vid)
+        {
+            var vendorMap = new Dictionary<string, string>
+        {
+            { "046D", "LOGITECH" }, // Y
+            { "045E", "MICROSOFT" },
+            { "1532", "RAZER" }, // Y
+            { "1E4E", "STEELSERIES" }, // Y
+            { "0B05", "ASUS" }, // Y
+            { "0C45", "MICRODIA" },
+            { "056E", "ELECOM" },
+            { "04F2", "CHICONY" },
+            { "054C", "SONY" },
+            { "28DE", "VALVE" },
+            { "1B1C", "CORSAIR"} // Y
+        };
+
+            return vendorMap.TryGetValue(vid, out var name) ? name : null;
+
+            //opens file
+            //var p = new Process();
+            //p.StartInfo.FileName = "notepad.exe";  // just for example, you can use yours.
+            //p.Start();
+        }
+
+        // Driver dictionary stores urls for each driver
+        static string GetDriverUrl(string driver)
+        {
+            var driverMap = new Dictionary<string, string>
+            {
+                {"NVIDIA", @"https://in.download.nvidia.com/GFE/GFEClient/3.28.0.417/GeForce_Experience_v3.28.0.417.exe" },
+                {"AMD", @"https://drivers.amd.com/drivers/installer/25.10/whql/amd-software-adrenalin-edition-25.5.1-minimalsetup-250513_web.exe" },
+                {"INTEL", @"https://dsadata.intel.com/installer" },
+                {"LOGITECH", @"https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.exe" },
+                {"RAZER", @"https://rzr.to/synapse-4-pc-download" },
+                {"CORSAIR", @"https://www3.corsair.com/software/CUE_V5/public/modules/windows/installer/Install%20iCUE.exe?_gl=1*1cff191*_gcl_au*MjA5NTE4Njk1OC4xNzQ5ODc0OTM3" },
+                {"STEELSERIES", @"https://steelseries.com/gg/downloads/gg/latest/windows" },
+                {"ASUS", @"https://dlcdnets.asus.com/pub/ASUS/mb/14Utilities/ArmouryCrateInstallTool.zip?model=Armoury%20Crate" }
+            };
+
+            return driverMap.TryGetValue(driver, out var url) ? url : null;
+        }
+
+        //downloads files
+        static void downloadHandler(string[] driverList, Label infoBox, CheckedListBox driverListBox)
+        {
+            infoBox.Text += "Loading...";
+            //gets username
+            string userNameRaw = System.Security.Principal.WindowsIdentity.GetCurrent().Name.Split('\\')[1];
+            Console.WriteLine("\nCurrent User: " + userNameRaw);
+
+            // Url and Filename varibles for webclient downloads
+            string activeUrl = null;
+            string activeFileName = null;
+
+            // Webclient init
+            WebClient webClient = new WebClient();
+            webClient.Headers.Add("User-Agent: Other");
+            webClient.Headers.Add("Referer", "https://www.amd.com/en/support/download/drivers.html");
+
+            
+            // Loop that tells webclient which drivers to download
+            foreach (string driver in driverList)
+                {
+                infoBox.Text += "\nDownloading " + driver + " drivers...";
+                infoBox.Update();
+                Console.WriteLine("\nDownloading " + driver + " drivers...");
+
+                    // gets driver url from dictionary
+                    activeUrl = GetDriverUrl(driver);
+
+                    // gets filename from url
+                    // razer doesnt have filename in url so this checks if razer and adds filename manually
+                    switch (driver)
+                    {
+                        case "RAZER":
+                            activeFileName = "RazerSynapseInstaller.exe";
+                            break;
+                        case "INTEL":
+                            activeFileName = "Intel-Driver-and-Support-Assistant-Installer.exe";
+                            break;
+                        case "CORSAIR":
+                            activeFileName = "Install-iCUE.exe";
+                            break;
+                        case "STEELSERIES":
+                            activeFileName = "SteelSeriesGG88.0.0Setup.exe";
+                            break;
+                        case "ASUS":
+                            activeFileName = "ArmouryCrateInstallTool.zip";
+                            break;
+                        default:
+                            activeFileName = activeUrl.Split('/')[activeUrl.Split('/').Length - 1];
+                            break;
+                    }
+                    // Download runs for each driver in list
+                    webClient.DownloadFile(activeUrl, @"C:\Users\" + userNameRaw + @"\Downloads\" + activeFileName);
+                    Console.WriteLine("Download Complete");
+                    infoBox.Text += "\nDownload Complete";
+            }
+            infoBox.Text += "\nCompleted All Downloads.";
+
+        }
+
+        
+
+        private void downloadBtn_Click(object sender, EventArgs e)
+        {
+            infoBox.Text = string.Empty;
+            List<string> checkedDriverList = new List<string>();
+
+            foreach (string s in driverListBox.CheckedItems)
+            {
+                checkedDriverList.Add(s);
+            }
+            string[] driverArray = checkedDriverList.ToArray();
+            downloadHandler(driverArray, infoBox, driverListBox);
+        }
+    }
+}
