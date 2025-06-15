9631b70b6e1c253e485a6ec51fcaf104dd19530c
bbrodo <15292@coderacademy.edu.au>
2025-06-15 11:22:02 +1000

    Fix: added check for headsets to input devices.

--- AutoDriverDownloader-2/AutoDriverDownloader.cs
+++ AutoDriverDownloader-2/AutoDriverDownloader.cs
@@ -95,7 +95,7 @@ namespace AutoDriverDownloader_2
 
                     // Checks if device is a mouse or keyboard
                     if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(deviceId) && (name.Contains("mouse") ||
-                         name.Contains("keyboard")))
+                         name.Contains("keyboard") || name.Contains("headset")))
                     {
                         Console.WriteLine($"Device: {name}");
                         infoBox.Text += $"\nDevice: {name}";
